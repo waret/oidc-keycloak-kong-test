cbf4985483c29959d2d64ff5712b4f82aa5bbd17
Waret Li <waret87@gmail.com>
2019-01-27 18:25:08 +0800

    update

--- keycloak-vue-oidc-foxtrot/src/services/AuthService.ts
+++ keycloak-vue-oidc-foxtrot/src/services/AuthService.ts
@@ -4,24 +4,24 @@ export default class AuthService {
     private userManager: UserManager;
 
     constructor() {
-        const AUTH0_DOMAIN: string = 'http://login.waret.net/auth/realms/jhipster';
-        const AUTH0_DOMAIN2: string = AUTH0_DOMAIN + '/protocol/openid-connect';
+        const AUTH0_DOMAIN_ISSUER: string = 'http://login.waret.net/auth/realms/jhipster';
+        const AUTH0_DOMAIN: string = AUTH0_DOMAIN_ISSUER + '/protocol/openid-connect';
 
         const settings: any = {
             userStore: new WebStorageStateStore({ store: window.localStorage }),
-            authority: AUTH0_DOMAIN,
+            authority: AUTH0_DOMAIN_ISSUER,
             client_id: 'web_app',
             redirect_uri: 'http://foxtrot.waret.net/callback.html',
             response_type: 'id_token token',
-            scope: 'openid profile',
+            scope: 'openid profile email',
             post_logout_redirect_uri: 'http://foxtrot.waret.net',
             filterProtocolClaims: true,
             metadata: {
-                issuer: AUTH0_DOMAIN,
-                authorization_endpoint: AUTH0_DOMAIN2 + '/auth',
-                userinfo_endpoint: AUTH0_DOMAIN2 + '/userinfo',
-                end_session_endpoint: AUTH0_DOMAIN2 + '/logout',
-                jwks_uri: AUTH0_DOMAIN2 + '/certs',
+                issuer: AUTH0_DOMAIN_ISSUER,
+                authorization_endpoint: AUTH0_DOMAIN + '/auth',
+                userinfo_endpoint: AUTH0_DOMAIN + '/userinfo',
+                end_session_endpoint: AUTH0_DOMAIN + '/logout',
+                jwks_uri: AUTH0_DOMAIN + '/certs',
             },
         };
 
@@ -38,5 +38,11 @@ export default class AuthService {
 
     public logout(): Promise<void> {
         return this.userManager.signoutRedirect();
+    }
+
+    public async isLoggedIn(): Promise<boolean> {
+        const user: User = await this.getUser();
+
+        return (user !== null && !user.expired);
     }
 }
