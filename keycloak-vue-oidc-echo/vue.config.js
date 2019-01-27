3d0923ca9d82bf350889e584173940f555426f50
Waret Li <waret87@gmail.com>
2019-01-27 18:36:58 +0800

    update

--- keycloak-vue-oidc-echo/vue.config.js
+++ keycloak-vue-oidc-echo/vue.config.js
@@ -2,7 +2,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   devServer: {
+    host: '172.16.123.1',
     port: 5004,
+    public: 'echo.waret.net',
     disableHostCheck: true
   },
   configureWebpack: {
--- keycloak-vue-oidc-foxtrot/vue.config.js
+++ keycloak-vue-oidc-foxtrot/vue.config.js
@@ -2,7 +2,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   devServer: {
+    host: '172.16.123.1',
     port: 5005,
+    public: 'foxtrot.waret.net',
     disableHostCheck: true
   },
   configureWebpack: {
